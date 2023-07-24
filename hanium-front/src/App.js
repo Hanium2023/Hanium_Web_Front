e5a6af5e894f1a8334c44dafbe454ae4c8a38188
choijihyeon <gus1043@gmail.com>
2023-07-25 01:56:42 +0900

    first init

---
+++ hanium-front/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Menu from "./component/Menu";
+import Main from "./component/Page/Main";
+import Check from "./component/Page/Check";
+import Setting from "./component/Page/Setting";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Menu />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/check" element={<Check />} />
+            <Route path="/setting" element={<Setting />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
