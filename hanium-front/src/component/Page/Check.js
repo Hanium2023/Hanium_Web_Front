bba5f7e8c4144084656612a924eace22c1f48205
choijihyeon <gus1043@gmail.com>
2023-09-18 23:09:05 +0900

    UI:font 크기

--- hanium-front/src/component/Page/Check.js
+++ hanium-front/src/component/Page/Check.js
@@ -140,16 +140,24 @@ export default function Check() {
             </Grid>
 
             <Grid item xs={8}>
-              <Item>
+              <Item style={{ height: "380px" }}>
                 <h2>각 가전 제품별 예상 전기 요금</h2>
                 <TableContainer component={Paper}>
                   <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                       <TableRow>
-                        <TableCell align="center">가전제품</TableCell>
-                        <TableCell align="center">위치</TableCell>
-                        <TableCell align="center">최종 사용일자/시각</TableCell>
-                        <TableCell align="center">예상 요금</TableCell>
+                        <TableCell align="center">
+                          <h3>가전제품</h3>
+                        </TableCell>
+                        <TableCell align="center">
+                          <h3>위치</h3>
+                        </TableCell>
+                        <TableCell align="center">
+                          <h3>최종 사용일자/시각</h3>
+                        </TableCell>
+                        <TableCell align="center">
+                          <h3>예상 요금</h3>
+                        </TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
@@ -160,12 +168,24 @@ export default function Check() {
                             "&:last-child td, &:last-child th": { border: 0 },
                           }}
                         >
-                          <TableCell align="center" component="th" scope="row">
+                          <TableCell
+                            align="center"
+                            component="th"
+                            scope="row"
+                            style={{ fontSize: "15px" }}
+                          >
                             {row.name}
                           </TableCell>
-                          <TableCell align="center">{row.location}</TableCell>
-                          {/* Render the fetched timestamps */}
-                          <TableCell align="center">
+                          <TableCell
+                            align="center"
+                            style={{ fontSize: "15px" }}
+                          >
+                            {row.location}
+                          </TableCell>
+                          <TableCell
+                            align="center"
+                            style={{ fontSize: "15px" }}
+                          >
                             {row.name === "모니터"
                               ? tvMonitorTimestamp
                               : row.name === "공기청정기"
@@ -174,7 +194,10 @@ export default function Check() {
                               ? LightTimestamp
                               : row.date}
                           </TableCell>
-                          <TableCell align="center">
+                          <TableCell
+                            align="center"
+                            style={{ fontSize: "15px" }}
+                          >
                             $
                             {(
                               (row.usageCount *
@@ -191,8 +214,14 @@ export default function Check() {
               </Item>
             </Grid>
             <Grid item xs={4}>
-              <Item>
-                <Weather style={{ width: "100%", height: "100%" }} />
+              <Item style={{ height: "380px" }}>
+                <Weather
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                  }}
+                />
               </Item>
             </Grid>
           </Grid>
